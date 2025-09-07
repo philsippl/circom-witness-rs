301c526fcc97baf1bd4d4aedf3efa2d79d0703bc
Philipp Sippl <philsippl@gmail.com>
2025-09-07 21:38:52 +0200

    env var for build

--- build.rs
+++ build.rs
@@ -3,18 +3,17 @@ use std::{env, fs, path::Path, process::Command};
 fn main() {
     if cfg!(feature = "build-witness") {
         let witness_cpp = env::var("WITNESS_CPP").unwrap();
+        let library_path = env::var("CIRCOM_LIBRARY_PATH");
         let circuit_file = Path::new(&witness_cpp);
         let circuit_name = circuit_file.file_stem().unwrap().to_str().unwrap();
 
-        let status = Command::new("/Users/ps/scm/github/iden3/circom/target/release/circom")
-            .args([
-                fs::canonicalize(circuit_file).unwrap().to_str().unwrap(),
-                "--c",
-                "-l",
-                "/Users/ps/scm/github/philsippl/semaphore-witness-example/src"
-            ])
-            .status()
-            .unwrap();
+        let mut cmd = Command::new("circom");
+        cmd.arg(fs::canonicalize(circuit_file).unwrap())
+            .arg("--c");
+        if library_path.is_ok() {
+            cmd.arg("-l").arg(fs::canonicalize(library_path.unwrap()).unwrap());
+        }
+        let status = cmd.status().unwrap();
         assert!(status.success());
 
         let cpp = Path::new("./")
